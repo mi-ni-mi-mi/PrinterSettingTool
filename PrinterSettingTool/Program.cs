220bb3ec6cedd438e85a2905d94a82001aab32bc
mi-ni-mi-mi <79532972+mi-ni-mi-mi@users.noreply.github.com>
2021-12-05 07:47:30 +0900

    Add files via upload

---
+++ PrinterSettingTool/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace PrinterSettingTool
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+
+        [STAThread]
+        static void Main()
+        {
+            if (!ProcessExists())
+            {
+                Application.Run(new Form1());
+            }
+        }
+
+
+        public static bool ProcessExists()
+        {
+            Process thisProcess = Process.GetCurrentProcess();
+            Process[] Processes = Process.GetProcessesByName(thisProcess.ProcessName);
+            int thisProcessId = thisProcess.Id;
+
+            foreach (Process aProcess in Processes)
+            {
+                if (aProcess.Id != thisProcessId)
+                {
+                    NativeMethods.ShowWindow(aProcess.MainWindowHandle, (int)SW.SHOWNORMAL);
+                    NativeMethods.SetForegroundWindow(aProcess.MainWindowHandle);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
